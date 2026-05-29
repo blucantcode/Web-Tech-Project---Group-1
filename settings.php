cc7a931d74e82ea60c6c2e45667ab2325df6ae95
Emily Armstrong <emilymarmstrong07@gmail.com>
2026-05-29 14:59:31 +1000

    created the settings.php page connected my database to the settings. ignore the test.php page i will delete that.

--- settings.php
+++ settings.php
@@ -1,13 +1,25 @@
 <?php
 
+// Store the database server name
 $host = "localhost";
+
+// Store the mysql username
 $user = "root";
+
+// Store the MySQL password - no password
 $password = "";
+
+// storing the name of the DB 
 $database = "project2";
 
+
+// Create a connection to the MySQL database
 $conn = mysqli_connect($host, $user, $password, $database);
 
+
+// Check if the connection failed
 if (!$conn) {
+       // Stop the program and display an error message
     die("Connection failed: " . mysqli_connect_error());
 }
 
