d09446ebc258f9eeb187a6513c02de171f26e204
Emily Armstrong <emilymarmstrong07@gmail.com>
2026-05-31 15:47:36 +1000

    having issues with my about page, just testing to see if something will work

--- settings.php
+++ settings.php
@@ -1,4 +1,11 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+
+
+<?php
 
 // Store the database server name
 $host = "localhost";
@@ -10,47 +17,46 @@ $user = "root";
 $password = "";
 
 
+
+
 // storing the name of the DB 
 $database = "project2";
-
 
 // Create a connection to the MySQL database
 $conn = mysqli_connect($host, $user, $password, $database);
 
-
 // Check if the connection failed
 if (!$conn) {
-       // Stop the program and display an error message
-    die("Connection failed: " . mysqli_connect_error());
-
-
-
-//Im missing the DB file for this so cannot run my managelogin screen! Once I have DB all good :D -Lotus
-// $database = "projet2";
-
-// $conn = mysqli_connect($host, $user, $password, $database);
-
-// if (!$conn) {
-//     die("Connection failed: " . mysqli_connect_error());
-// }
-
-//Manage.php
-$database2 = "manage";
-
-$connManage = mysqli_connect($host, $user, $password, $database2);
-
-if (!$connManage) {
-    die("Manage connection failed: " . mysqli_connect_error());
+    // Stop the program and display an error message
+    die("Project DB connection failed: " . mysqli_connect_error());
 }
 
-//jobspage.php
+
+
+
+// Im missing the DB file for this so cannot run my managelogin screen!
+//$database2 = "manage";
+
+// Create connection for manage DB
+//$connManage = mysqli_connect($host, $user, $password, $database2);
+
+// Check connection
+//if (!$connManage) {
+//    die("Manage connection failed: " . mysqli_connect_error());
+//}
+
+
+
+
+ jobspage.php
 $database3 = "jobstable_db";
 
-$connManage = mysqli_connect($host, $user, $password, $database3);
+ Create connection for jobs DB
+$connJobs = mysqli_connect($host, $user, $password, $database3);
 
-if (!$connManage) {
-    die("Manage connection failed: " . mysqli_connect_error());
-
+ Check connection
+if (!$connJobs) {
+   die("Jobs connection failed: " . mysqli_connect_error());
 }
 
 ?>
