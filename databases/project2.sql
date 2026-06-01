cdc19ec605f73c88354c3e880cb28fc1352470f1
blucantcode <105138731@student.swin.edu.au>
2026-06-01 17:12:29 +1000

    Update project2.sql
    Added contribution 2

--- databases/project2.sql
+++ databases/project2.sql
@@ -42,7 +42,7 @@ CREATE TABLE `about` (
 
 INSERT INTO `about` (`id`, `name`, `student_id`, `contribution-1`, `contribution-2`, `quote`) VALUES
 (1, 'Emily Armstrong', '106505064', 'Worked on the About page and collaborated on CSS styling', 'Database settings\nCreate about table and update', 'Quote: \"Noli solliciti esse, omnia tandem bene eveniunt.\"\n(Don\'t worry, it all works out in the end) -Latin'),
-(2, 'Lotus Allan', '105138731', 'Developed the index page and collaborated on CSS styling', '', 'Quote: \"Vita revera est valde simplex, non necesse est eam tam complicatam facere.\"\n(Life is really very simple, there\'s no need to make it so complicated.) - Latin'),
+(2, 'Lotus Allan', '105138731', 'Developed the index page and collaborated on CSS styling', 'Created Manager Login and Manager Dashboard', 'Quote: \"Vita revera est valde simplex, non necesse est eam tam complicatam facere.\"\n(Life is really very simple, there\'s no need to make it so complicated.) - Latin'),
 (3, 'Phoebe Anastasiou', '106509600', 'Created the Jobs page and collaborated on CSS styling', 'Created a functioning search feature', 'Quote: \"Carpe diem\"\n(Seize the day) - Latin'),
 (4, 'Krisha Upadhyay', '106513368', 'Built the Apply page and collaborated on CSS styling', '', 'Quote: \"sicut ad eam\"\n(Just do it) - Latin');
 
